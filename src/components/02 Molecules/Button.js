0de6fac35d79a8a29432d9fbba24277155863586
coatriver <sylvain.jacot@hotmail.be>
2020-10-01 07:46:51 +0200

    a tag nested into another a, fixed. Replaced with the span option for the button component

--- src/components/02 Molecules/Button.js
+++ src/components/02 Molecules/Button.js
@@ -50,6 +50,62 @@ ${(props) => {
 
 ${(props) => {
     return (
+        props.lightmode &&
+        css`
+        border-color: ${colorsRoles.DarkGrey};
+        & > div {
+        color: ${colorsRoles.DarkGrey};
+        & > svg {
+        fill: ${colorsRoles.DarkGrey};
+        }
+        }
+        `
+    );
+}}
+
+`;
+export const StyledSpan = styled.span`
+position: relative;
+display: inline-block;
+width: fit-content;
+height: fit-content;
+padding: 16px;
+margin-top: 8px;
+margin-bottom: 16px;
+border-radius: 8px;
+color: ${colorsRoles.White};
+background-color: transparent;
+border: 1px solid ${colorsRoles.White};  
+& div > svg {
+fill: ${colorsRoles.White};
+}  
+${media.desktop`
+margin-top: 24px;
+padding: 24px;
+margin-bottom: 0px;
+&:hover {
+        & svg {
+        margin-left: 32px;
+        transition: ${transitions.basic1};
+        }
+    }
+`}
+    
+    
+${(props) => {
+    return (
+        props.small &&
+        css`
+      padding: 8px 16px 8px 16px;
+      ${media.desktop`
+      padding: 8px 16px 8px 16px;
+      `}
+          `
+    );
+}}
+
+${(props) => {
+    return (
         props.LightMode &&
         css`
         border-color: ${colorsRoles.DarkGrey};
@@ -81,10 +137,23 @@ transition: ${transitions.basic1};
 
 
 
-const Button = ({ pathname, label, small, lightmode}) => {
+const Button = ({ pathname, label, small, lightmode, isspan}) => {
+
 
     return (
+        <>
 
+        {
+            isspan ?
+
+            <StyledSpan  small={small} lightmode={lightmode}>
+                <Content>
+                    {label ? label : "Default"}
+                    <IcArrow IconSize={"24px"} SizeBis={"100%"}/>
+                </Content>
+            </StyledSpan>
+
+                :
 
             <StyledLink to={pathname} small={small} lightmode={lightmode}>
                 <Content>
@@ -92,6 +161,9 @@ const Button = ({ pathname, label, small, lightmode}) => {
                     <IcArrow IconSize={"24px"} SizeBis={"100%"}/>
                 </Content>
             </StyledLink>
+
+        }
+        </>
 
     );
 };
