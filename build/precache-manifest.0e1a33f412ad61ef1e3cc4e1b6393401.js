1ba50a4c49a87977fae6a7f05d8c02f5d03139b5
coatriver <sylvain.jacot@hotmail.be>
2020-10-05 08:55:58 +0200

    build added

---
+++ build/precache-manifest.0e1a33f412ad61ef1e3cc4e1b6393401.js
@@ -0,0 +1,622 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d76934f5fcfce3ba0932bfea466496",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e221777b78e6ca2e6e72",
+    "url": "/static/css/main.2160750c.chunk.css"
+  },
+  {
+    "revision": "6c238219e102de04866c",
+    "url": "/static/js/2.06b2ee72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.06b2ee72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e221777b78e6ca2e6e72",
+    "url": "/static/js/main.8016e021.chunk.js"
+  },
+  {
+    "revision": "da903b2984fdcfa129a5",
+    "url": "/static/js/runtime-main.4749a357.js"
+  },
+  {
+    "revision": "93bc833c2ac584195457be32694ca3d5",
+    "url": "/static/media/001_EATBXL_FOODDUO_BAO.93bc833c.png"
+  },
+  {
+    "revision": "0ad7d143e6b7dc313767f2d699aa039c",
+    "url": "/static/media/021_EATBXL_FOOD_ChampignonSansaru.0ad7d143.png"
+  },
+  {
+    "revision": "dabc35e0621ea37f229d1d1e672a9ecc",
+    "url": "/static/media/041_EATBXL_FOOD_Fromages_Arpin.dabc35e0.png"
+  },
+  {
+    "revision": "488a6473adc27daf8de7c35efedee253",
+    "url": "/static/media/091_EATBXL_FOOD_Homard.488a6473.png"
+  },
+  {
+    "revision": "6835d7fe23b32c58f653b948adab7259",
+    "url": "/static/media/101_EATBXL_FOOD_Mais.6835d7fe.png"
+  },
+  {
+    "revision": "d61ab58e15386527be7d0cd055aedbba",
+    "url": "/static/media/121_EATBXL_FOOD_PimentColores.d61ab58e.png"
+  },
+  {
+    "revision": "8b1ae38ee700a7d240500e09d236955b",
+    "url": "/static/media/141_EATBXL_FOOD_PommeDeTerre.8b1ae38e.png"
+  },
+  {
+    "revision": "1e24f15ffd08166bf3dbd122bd33204a",
+    "url": "/static/media/151_EATBXL_FOOD_QueueBoeuf.1e24f15f.png"
+  },
+  {
+    "revision": "8989f7d0bbbc321cbafc35bcf51fdd07",
+    "url": "/static/media/171_EATBXL_FOOD_Coco.8989f7d0.png"
+  },
+  {
+    "revision": "6cfe217a87806496e34103361e06bd05",
+    "url": "/static/media/191_EATBXL_FOOD_DUO_Graines.6cfe217a.png"
+  },
+  {
+    "revision": "1e9c4eff8a9657741c78165f84689dbb",
+    "url": "/static/media/241_EATBXL_FOOD_Poulpe.1e9c4eff.png"
+  },
+  {
+    "revision": "00e6bdc95a27f99c5301ae52c7915b26",
+    "url": "/static/media/241_EATBXL_FOOD_Thumbnail.00e6bdc9.png"
+  },
+  {
+    "revision": "0e96eed9a24fd0dd89071e63b6b3f38f",
+    "url": "/static/media/8pt.0e96eed9.svg"
+  },
+  {
+    "revision": "73960f324b365a15f32b35bba679d79d",
+    "url": "/static/media/AgileDeve.73960f32.svg"
+  },
+  {
+    "revision": "6126cc9d8af5d56fe58203aa3c724579",
+    "url": "/static/media/Art_Gallery_Mockup.6126cc9d.png"
+  },
+  {
+    "revision": "9e1a5cadb447dbd67da4536ce1539743",
+    "url": "/static/media/Bootstrap.9e1a5cad.svg"
+  },
+  {
+    "revision": "b25d5636fbe8a40872279a9d563ffa86",
+    "url": "/static/media/ClavieDecorative.b25d5636.png"
+  },
+  {
+    "revision": "14a4be3a121075875e672757235a3dca",
+    "url": "/static/media/ClavieFolio01.14a4be3a.png"
+  },
+  {
+    "revision": "603f1f62506fe15b44084231534de9f8",
+    "url": "/static/media/ClavieFolio02.603f1f62.jpg"
+  },
+  {
+    "revision": "4790e155fd84308bfa1c7e16f52c56cd",
+    "url": "/static/media/ClioGoldbrenner.4790e155.png"
+  },
+  {
+    "revision": "1e541a9cd4687192d7507303ae9e5903",
+    "url": "/static/media/CrossBrowser.1e541a9c.svg"
+  },
+  {
+    "revision": "f5b8ee19209e65420fdbef9e1fd9c160",
+    "url": "/static/media/DecorativePicture.f5b8ee19.png"
+  },
+  {
+    "revision": "822c9990d5ed9c91d2abbf7616e46b61",
+    "url": "/static/media/EatDeco.822c9990.png"
+  },
+  {
+    "revision": "149e1b0a1d322b114112e4c190500833",
+    "url": "/static/media/Eat_BeforeAfter.149e1b0a.png"
+  },
+  {
+    "revision": "4fc9bab2daeaf7514e35ba8c13404721",
+    "url": "/static/media/Evergreen-folio_Desktop.4fc9bab2.png"
+  },
+  {
+    "revision": "0264aef5aa816356a0292462afaada9a",
+    "url": "/static/media/Evergreen-folio_Mobile.0264aef5.png"
+  },
+  {
+    "revision": "884886f19d954e6c220fe46204333147",
+    "url": "/static/media/FirstView.884886f1.png"
+  },
+  {
+    "revision": "d84d6bf819b86915cb3fd21e6c4e44d7",
+    "url": "/static/media/FirstView.d84d6bf8.png"
+  },
+  {
+    "revision": "f8e6170047455d8161c81d7ffb62262c",
+    "url": "/static/media/FirstView.f8e61700.png"
+  },
+  {
+    "revision": "1f8ab977d5f6dd6235896258951d06cd",
+    "url": "/static/media/Fox.1f8ab977.png"
+  },
+  {
+    "revision": "d0793eaff2491aba1c28c5f1d9ac1005",
+    "url": "/static/media/FredSablon.d0793eaf.svg"
+  },
+  {
+    "revision": "cae2c44670dff1e7104627250de042e3",
+    "url": "/static/media/GraphicTablet.cae2c446.svg"
+  },
+  {
+    "revision": "7bb0c81512912932d43b61805d62165a",
+    "url": "/static/media/H_Almaha-Deco.7bb0c815.png"
+  },
+  {
+    "revision": "d54da19f5dc4d33ce627e9d449e2162d",
+    "url": "/static/media/H_Almaha-Folio01.d54da19f.png"
+  },
+  {
+    "revision": "fcefe2ed8edb9a0b4e124bd48eaaf619",
+    "url": "/static/media/H_Almaha-Folio02.fcefe2ed.png"
+  },
+  {
+    "revision": "8391bade8bb9af41e048093740342d11",
+    "url": "/static/media/H_Almaha-Folio03.8391bade.png"
+  },
+  {
+    "revision": "593cfc605821d5141fa2c947d2dd0c75",
+    "url": "/static/media/H_Almaha-Folio04.593cfc60.png"
+  },
+  {
+    "revision": "e2ac13610d8e374e430ac74077db8491",
+    "url": "/static/media/H_Almaha-Folio05.e2ac1361.png"
+  },
+  {
+    "revision": "287c8cd67fb512dd2adaef24b343dcc3",
+    "url": "/static/media/H_Almaha-Folio06.287c8cd6.png"
+  },
+  {
+    "revision": "4b13777b03b7afba9b181be72ccb711a",
+    "url": "/static/media/H_Almaha-Folio07.4b13777b.png"
+  },
+  {
+    "revision": "9ed0f1f5d2e47694b2a9bb5a8fee8e48",
+    "url": "/static/media/H_Almaha-Folio08.9ed0f1f5.png"
+  },
+  {
+    "revision": "eefcb411f07df8388501b71e9d90aee1",
+    "url": "/static/media/H_Almaha-Folio09.eefcb411.png"
+  },
+  {
+    "revision": "1d81220451009a727170896afdcb2d7e",
+    "url": "/static/media/H_Almaha-Folio10.1d812204.png"
+  },
+  {
+    "revision": "051bad317cda147a2b5c632d1a8f437b",
+    "url": "/static/media/H_Almaha-Thumbnail.051bad31.png"
+  },
+  {
+    "revision": "4370b06cedc7a4fd63cf1f482df5a9c4",
+    "url": "/static/media/Horse.4370b06c.png"
+  },
+  {
+    "revision": "6013c3e1d20d19c563d64a79a2cd9ff5",
+    "url": "/static/media/Html5.6013c3e1.svg"
+  },
+  {
+    "revision": "f2abdb883bb85d5e9076a41caf948e02",
+    "url": "/static/media/Kia-folio.f2abdb88.png"
+  },
+  {
+    "revision": "1390f6f7ed1e787354ce99d53d7ffd78",
+    "url": "/static/media/Kia.1390f6f7.svg"
+  },
+  {
+    "revision": "1a43838309b4c912c1286722388195c3",
+    "url": "/static/media/KitCross.1a438383.svg"
+  },
+  {
+    "revision": "1cf5acf8d2623a05686cbcc33821d55d",
+    "url": "/static/media/Leaves.1cf5acf8.png"
+  },
+  {
+    "revision": "cc66d26959a9afed05e5e577d60292c6",
+    "url": "/static/media/LightRoomClassic.cc66d269.svg"
+  },
+  {
+    "revision": "c45e74a6589a3795dfa5478a1b232a53",
+    "url": "/static/media/Mazda.c45e74a6.svg"
+  },
+  {
+    "revision": "46407e1f8b43657fb3996c586ec1e1c6",
+    "url": "/static/media/Merco-BA.46407e1f.png"
+  },
+  {
+    "revision": "c340cb4679d6c2ec3b4e7d3c2d7256a4",
+    "url": "/static/media/Merco-Deco.c340cb46.png"
+  },
+  {
+    "revision": "ea2c3ed8f97c88a66f031bc2f5dc2c72",
+    "url": "/static/media/Merco-Folio01.ea2c3ed8.png"
+  },
+  {
+    "revision": "8ac0a2bdcacaa86858123cb4f39738d4",
+    "url": "/static/media/Merco-Folio02.8ac0a2bd.png"
+  },
+  {
+    "revision": "51d4e83b59f6fec5ebc860054619fc53",
+    "url": "/static/media/Merco-Main.51d4e83b.png"
+  },
+  {
+    "revision": "c62f2cf67cf140d7c6b4fc66a1cd039a",
+    "url": "/static/media/Merco-Thumbnail.c62f2cf6.png"
+  },
+  {
+    "revision": "9ed78f7f911d589c8634e971a32982f4",
+    "url": "/static/media/Parentreprise-deco.9ed78f7f.png"
+  },
+  {
+    "revision": "f2ed3e395acafb957533270bc1cda7a2",
+    "url": "/static/media/Parentreprise-folio.f2ed3e39.png"
+  },
+  {
+    "revision": "3f8a7c189ecbda81c8ca92392b8a1178",
+    "url": "/static/media/Parentreprise.3f8a7c18.svg"
+  },
+  {
+    "revision": "142f130e76e14c2dbc60cf12cb8d3e8f",
+    "url": "/static/media/Parrot.142f130e.png"
+  },
+  {
+    "revision": "436c415286fe4c0eb99ad75998b22d61",
+    "url": "/static/media/Photoshop.436c4152.svg"
+  },
+  {
+    "revision": "1d5cb73d99cc8ce9d8684aef6845500a",
+    "url": "/static/media/PrimeMinister-Deco.1d5cb73d.png"
+  },
+  {
+    "revision": "77cc715d45688884230029ba6c3107ae",
+    "url": "/static/media/PrimeMinister-Thumbnail.77cc715d.png"
+  },
+  {
+    "revision": "75b8ae4320271ab61f9a0a79b631603c",
+    "url": "/static/media/PrimeMinister-beforeafter.75b8ae43.png"
+  },
+  {
+    "revision": "abe68d5087a83af9deee96424dc9fb1e",
+    "url": "/static/media/PrimeMinisterFolio01.abe68d50.png"
+  },
+  {
+    "revision": "d200714082bf5a43bca328d8494d3ea0",
+    "url": "/static/media/PrimeMinisterFolio02.d2007140.png"
+  },
+  {
+    "revision": "6f5afec07e22ac79ac6c4519627675d3",
+    "url": "/static/media/PrimeMinisterMain.6f5afec0.png"
+  },
+  {
+    "revision": "66bf336f9bbf117216aa157af37599f1",
+    "url": "/static/media/Principle.66bf336f.svg"
+  },
+  {
+    "revision": "73e6e5a4e95d9bfe6d6e7f72219fa457",
+    "url": "/static/media/ProflPic.73e6e5a4.png"
+  },
+  {
+    "revision": "b78b5ac36052bf0144528a60841ea4f4",
+    "url": "/static/media/React.b78b5ac3.svg"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "/static/media/Roboto-Bold.08cb8f79.woff"
+  },
+  {
+    "revision": "1d77cf804e1c37bcb4bd0b7fc2a70208",
+    "url": "/static/media/Roboto-LightItalic.1d77cf80.woff"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/static/media/Roboto-Regular.94dac78e.woff"
+  },
+  {
+    "revision": "9a50ff392a5a1c464d054e100106db62",
+    "url": "/static/media/SUVDAYS_Posters_Landscape_A3.9a50ff39.png"
+  },
+  {
+    "revision": "f8065f703216d3fe2f440ec86665d3ed",
+    "url": "/static/media/Samsung.f8065f70.svg"
+  },
+  {
+    "revision": "acad63c71bae53b1388af0f388ddef37",
+    "url": "/static/media/Samsung2_desktop.acad63c7.png"
+  },
+  {
+    "revision": "47e4d4fedc0dbb707faefc883270e471",
+    "url": "/static/media/Samsung2_mobile.47e4d4fe.png"
+  },
+  {
+    "revision": "485e252993da9cea871d6163b08781aa",
+    "url": "/static/media/Samsung3_desktop.485e2529.png"
+  },
+  {
+    "revision": "6138bb2c13fbcd76cbd1c660f2b4d2b0",
+    "url": "/static/media/Samsung3_mobile.6138bb2c.png"
+  },
+  {
+    "revision": "9770980c46e00b436e8549ae3d75cbf4",
+    "url": "/static/media/SamsungGuidelines.9770980c.svg"
+  },
+  {
+    "revision": "5d7ce39230edfb26029adf69e49d2ee3",
+    "url": "/static/media/Samsung_desktop.5d7ce392.png"
+  },
+  {
+    "revision": "3bb1dfb55b448939dd53d674027a5d13",
+    "url": "/static/media/Samsung_mobile.3bb1dfb5.png"
+  },
+  {
+    "revision": "95a11e1c9bc41a92c35dd5469a3e9d3d",
+    "url": "/static/media/Sass.95a11e1c.svg"
+  },
+  {
+    "revision": "16bc50c02fcb9e0beacd0036ffc4465b",
+    "url": "/static/media/Signal.16bc50c0.png"
+  },
+  {
+    "revision": "8ecf4680afb81024c658cd12e8e01a00",
+    "url": "/static/media/Sketch.8ecf4680.svg"
+  },
+  {
+    "revision": "26009772be8279d33cd312c3fdf97541",
+    "url": "/static/media/Streching.26009772.svg"
+  },
+  {
+    "revision": "f2fb8ce3d4e9d6b9eac4afbc5cddf51e",
+    "url": "/static/media/THEHOTEL-BA.f2fb8ce3.png"
+  },
+  {
+    "revision": "8c16c85ca582d9105b2b880bbf7251a5",
+    "url": "/static/media/THEHOTEL-DECO.8c16c85c.png"
+  },
+  {
+    "revision": "4ca0498a40dda8b2f2d3fc54d5cf7257",
+    "url": "/static/media/THEHOTEL-Folio01.4ca0498a.png"
+  },
+  {
+    "revision": "84c3f7fc8a4220eda4434e7b3c199b36",
+    "url": "/static/media/THEHOTEL-Folio02.84c3f7fc.png"
+  },
+  {
+    "revision": "00729c3c85d3379e4914a5f7b519a408",
+    "url": "/static/media/THEHOTEL-Folio03.00729c3c.png"
+  },
+  {
+    "revision": "ce8b826b2a23c6965465f83872d1a0e4",
+    "url": "/static/media/THEHOTEL-Folio04.ce8b826b.png"
+  },
+  {
+    "revision": "faed8a6ab08ae77268fa5fb8dab8eb1b",
+    "url": "/static/media/THEHOTEL-Folio05.faed8a6a.png"
+  },
+  {
+    "revision": "a8a32474f44f05cbc0b60ededcf09312",
+    "url": "/static/media/THEHOTEL-Folio06.a8a32474.png"
+  },
+  {
+    "revision": "a56dfe53495e169f725bf6d97b1cb914",
+    "url": "/static/media/THEHOTEL-Folio07.a56dfe53.png"
+  },
+  {
+    "revision": "2944c40d42b3a2856f3a6e76acc2ec9f",
+    "url": "/static/media/THEHOTEL-Thumbnail.2944c40d.png"
+  },
+  {
+    "revision": "8f4f38a05dfd02b169c730d09b415056",
+    "url": "/static/media/Telenet.8f4f38a0.svg"
+  },
+  {
+    "revision": "c18b2348e7587fe964c76988251e6962",
+    "url": "/static/media/TelenetFolio02.c18b2348.jpg"
+  },
+  {
+    "revision": "e3153ca7f5f96e89388b03ac0a405952",
+    "url": "/static/media/TelenetFolio03.e3153ca7.jpg"
+  },
+  {
+    "revision": "49731b976001ce30623690863f83f4a8",
+    "url": "/static/media/TelenetFolio04.49731b97.jpg"
+  },
+  {
+    "revision": "d843bad584a13c702aa6fdd3727fc426",
+    "url": "/static/media/Telenetfolio01.d843bad5.gif"
+  },
+  {
+    "revision": "ae605d9747a2b2dacd45ce09b1a4863e",
+    "url": "/static/media/Trello.ae605d97.svg"
+  },
+  {
+    "revision": "5492d0d901a95502c29cecb1409a5415",
+    "url": "/static/media/WTF-Deco.5492d0d9.png"
+  },
+  {
+    "revision": "ed3ea9c4a1de916ce30520a93408583b",
+    "url": "/static/media/WTF-Folio01.ed3ea9c4.png"
+  },
+  {
+    "revision": "10f1ae4695c4b3802c0fc8734ba8d2f8",
+    "url": "/static/media/WTF-Folio02.10f1ae46.png"
+  },
+  {
+    "revision": "780a1dfe1fc5cec012bd2d4a5b01af98",
+    "url": "/static/media/WTF-Folio03.780a1dfe.png"
+  },
+  {
+    "revision": "ecbf5b677774b1f19de2e59ea859d45c",
+    "url": "/static/media/WTF-Folio04.ecbf5b67.png"
+  },
+  {
+    "revision": "0aa0912fb681f8c3e90d3b6630efd238",
+    "url": "/static/media/WTF-Folio05.0aa0912f.png"
+  },
+  {
+    "revision": "35f05948c142cbc65ed786abbc04a877",
+    "url": "/static/media/WTF-Folio06.35f05948.png"
+  },
+  {
+    "revision": "e7b7aecc27639a34648549c1cc54636b",
+    "url": "/static/media/WTF-Folio07.e7b7aecc.png"
+  },
+  {
+    "revision": "bce83beb305268b4ab6435e0553d042d",
+    "url": "/static/media/WTF-Folio08.bce83beb.png"
+  },
+  {
+    "revision": "bc5403f32f0fe1832a3abe1f20bd2ab9",
+    "url": "/static/media/WTF-Folio09.bc5403f3.png"
+  },
+  {
+    "revision": "7a18107760c9246af1e608d8057d6f10",
+    "url": "/static/media/WTF-Folio10.7a181077.png"
+  },
+  {
+    "revision": "0bcc33331336ba1c73393aed88f75044",
+    "url": "/static/media/WTF-Folio11.0bcc3333.png"
+  },
+  {
+    "revision": "78aefe6f7aca6446cae68c68ae71575b",
+    "url": "/static/media/WTF-Folio12.78aefe6f.png"
+  },
+  {
+    "revision": "abd133e2e506148736180c9286c1cde9",
+    "url": "/static/media/WTF-Thumbnail.abd133e2.png"
+  },
+  {
+    "revision": "1b9f25d6b6c5cc736e7dd7be97af4a3e",
+    "url": "/static/media/atomicdesign.1b9f25d6.svg"
+  },
+  {
+    "revision": "75928d1b5ad147739a332796375c203e",
+    "url": "/static/media/break.75928d1b.svg"
+  },
+  {
+    "revision": "4c51e0c94dfb37e9cb1ca8d4663c0693",
+    "url": "/static/media/clavie_thumbnail.4c51e0c9.png"
+  },
+  {
+    "revision": "a92a190d4af88748519259855e12f747",
+    "url": "/static/media/css3.a92a190d.svg"
+  },
+  {
+    "revision": "a2d0b9ab646ae2c6ab474c0af4ebce0d",
+    "url": "/static/media/deco-kia.a2d0b9ab.png"
+  },
+  {
+    "revision": "7e2babb516379a6ed39140017159e7a9",
+    "url": "/static/media/decorative-samung.7e2babb5.png"
+  },
+  {
+    "revision": "79d9870399894b630dc95a1486bf0cd8",
+    "url": "/static/media/decorativeTelenet.79d98703.jpg"
+  },
+  {
+    "revision": "3e2c2387ac38719c2edbf4ef4334543d",
+    "url": "/static/media/exteriors_eat_brussels.3e2c2387.png"
+  },
+  {
+    "revision": "59b701823e32050cdaecbaf3de1d5bcd",
+    "url": "/static/media/git.59b70182.svg"
+  },
+  {
+    "revision": "2338d1fdef62af6887a5a2d3a7b116a0",
+    "url": "/static/media/header-Parentreprise.2338d1fd.png"
+  },
+  {
+    "revision": "134c4ec5d007c344e4e22767374adb22",
+    "url": "/static/media/header-clavie.134c4ec5.png"
+  },
+  {
+    "revision": "bc13054ea7954be644e8a82e84a27261",
+    "url": "/static/media/header-evergreen.bc13054e.png"
+  },
+  {
+    "revision": "84985b08ae0230122d5cec8ec7195062",
+    "url": "/static/media/header-kia.84985b08.png"
+  },
+  {
+    "revision": "721ffaa45f527e1d26047793fc848f39",
+    "url": "/static/media/header-samsung.721ffaa4.png"
+  },
+  {
+    "revision": "1614c6f1bf2e1734544226826c06c8e3",
+    "url": "/static/media/header-sosmart.1614c6f1.png"
+  },
+  {
+    "revision": "b956b24103510036453c28f14cc632e9",
+    "url": "/static/media/header-telenet.b956b241.png"
+  },
+  {
+    "revision": "3bfe88917b7acd314f55814a1f2b60dd",
+    "url": "/static/media/invision.3bfe8891.svg"
+  },
+  {
+    "revision": "a173e45f6c90255e42189659a6f37ace",
+    "url": "/static/media/javascript.a173e45f.svg"
+  },
+  {
+    "revision": "8eee9194022f6c000ec02219b6c7ad66",
+    "url": "/static/media/kia-first.8eee9194.png"
+  },
+  {
+    "revision": "f02641ad57dade5acd6e00fccdc2f6ba",
+    "url": "/static/media/lovethegarden.f02641ad.svg"
+  },
+  {
+    "revision": "bf399b8db83309357c5b2a3384d57ae7",
+    "url": "/static/media/pixelperfect.bf399b8d.svg"
+  },
+  {
+    "revision": "ad2d1f730a5a548535ec6994ce37131c",
+    "url": "/static/media/rawSketch.ad2d1f73.svg"
+  },
+  {
+    "revision": "6d302dec61bf0a7946d6889f88bd55e6",
+    "url": "/static/media/sosmart-folio01.6d302dec.png"
+  },
+  {
+    "revision": "07ea2aae1304b7e13b6ed03c69d76f89",
+    "url": "/static/media/sosmart_thumbnail.07ea2aae.png"
+  },
+  {
+    "revision": "3e9e14fca782a80785ce7f0f5f676b84",
+    "url": "/static/media/sosmartdeco.3e9e14fc.png"
+  },
+  {
+    "revision": "5d76c06cf027568b758a51e3b5e26693",
+    "url": "/static/media/star.5d76c06c.png"
+  },
+  {
+    "revision": "51af637bb500dd2253157a703639d038",
+    "url": "/static/media/steeringwheel.51af637b.png"
+  },
+  {
+    "revision": "e4eb08233d0ebdcc7c8a4656c1c467b4",
+    "url": "/static/media/styledcomponents.e4eb0823.png"
+  },
+  {
+    "revision": "60f192ebd2a6a0382d936a3def1b94f3",
+    "url": "/static/media/thumbnail-Parentreprise.60f192eb.png"
+  },
+  {
+    "revision": "f3c4dc2319037c7b79a3d11dcb67c06f",
+    "url": "/static/media/web-design.f3c4dc23.svg"
+  },
+  {
+    "revision": "70396daf70ab1853b02ec3371c9b4bc2",
+    "url": "/static/media/zeplin.70396daf.svg"
+  }
+]);
