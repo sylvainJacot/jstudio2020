6792960e8bcb8182b4452e859bde745c79b34e4b
coatriver <sylvain.jacot@hotmail.be>
2020-10-28 17:02:11 +0100

    Tab sliding codesanboxe added

--- src/components/01 Atoms/codesandboxes.js
+++ src/components/01 Atoms/codesandboxes.js
@@ -1,6 +1,7 @@
 import TodolistImg from "../../media/img/Projects/02_Creative Developer/ToDoList.png";
 import ButtonComponentImg from "../../media/img/Projects/02_Creative Developer/ButtonComponent.png";
 import CounterIMG from "../../media/img/Projects/02_Creative Developer/Counter.png";
+import SlidingButton from "../../media/img/Projects/02_Creative Developer/SlidingButton.png";
 
 export const codesandboxes = [
     {
@@ -10,16 +11,22 @@ export const codesandboxes = [
         href: "https://codesandbox.io/s/lucid-colden-08mq3",
     },
     {
-        name: "Button Component",
-        src: ButtonComponentImg,
-        alt: "Button component link to CodeSandBox",
-        href: "https://codesandbox.io/s/button-component-02ecz",
+        name: "Sliding Tab",
+        src: SlidingButton,
+        alt: "Sliding Tab link to CodeSandBox",
+        href: "https://codesandbox.io/s/slidding-button-tabs-lgx96?file=/components/01_Atoms/TabButtons-Data.js",
     },
     {
         name: "Counter",
         src: CounterIMG,
         alt: "Coutner link to CodeSandBox",
         href: "https://codesandbox.io/s/thirsty-breeze-ynk2p",
+    },
+    {
+        name: "Button Component",
+        src: ButtonComponentImg,
+        alt: "Button component link to CodeSandBox",
+        href: "https://codesandbox.io/s/button-component-02ecz",
     },
 ]
 
